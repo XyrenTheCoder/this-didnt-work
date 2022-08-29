ffd0ba473ecd4e1372cf17fb1e2cc31338bd4438
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-29 13:17:12 +0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,16 +1,16 @@
-;
+;e
   %macro print 2
     mov eax, 4
     mov ebx, 1
-    mov ecx, %1
-    mov edx, %2
+    mov ecx, %1 ;msg
+    mov edx, %2 ;len
     int 0x80
   %endmacro
   
   %macro input 1
     mov eax, 3
     mov ebx, 2
-    mov ecx, %1
+    mov ecx, %1 ;input
     mov edx, 5
     int 0x80
    
@@ -22,7 +22,9 @@
 section .data
   out1 db 'input function:'
   len1 equ $-out1
+  
 section .text
   global _start
   
 _start:
+  print out1, len1
