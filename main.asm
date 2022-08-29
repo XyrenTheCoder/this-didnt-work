0107a8d9cdaa6bc2066ecf5eef9fa2adc2882aeb
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-29 13:05:48 +0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -1,1 +1,20 @@
 ;
+  %macro print 2
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, %1
+    mov edx, %2
+    int 0x80
+  %endmacro
+  
+  %macro exit 0
+    mov eax, 1
+    int 0x80
+  %endmacro
+  
+section .data
+
+section .text
+  global _start
+  
+_start:
