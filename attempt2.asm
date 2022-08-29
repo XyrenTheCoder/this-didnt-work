96675ca291c51bd1d02397dd4b96b1eca88a902d
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-29 17:15:10 +0800

    Update attempt2.asm

--- attempt2.asm
+++ attempt2.asm
@@ -1,3 +1,9 @@
+SYS_EXIT  equ 1
+SYS_READ  equ 3
+SYS_WRITE equ 4
+STDIN     equ 0
+STDOUT    equ 1
+
 section .data
 
 section .bss
@@ -8,3 +14,6 @@ section .text
 _start:
 
 exit:
+  mov eax, SYS_EXIT   
+  xor ebx, ebx 
+  int 0x80
