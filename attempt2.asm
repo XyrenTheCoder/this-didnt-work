5a41209860e769ab8166cbd0e9aaf8d8ad6bc6c6
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-08-29 17:20:41 +0800

    Update attempt2.asm

--- attempt2.asm
+++ attempt2.asm
@@ -23,7 +23,36 @@ section .text
   global _start
   
 _start:
+  mov eax, SYS_WRITE         
+  mov ebx, STDOUT         
+  mov ecx, msg1         
+  mov edx, len1 
+  int 0x80  
+  
+  mov eax, SYS_READ 
+  mov ebx, STDIN  
+  mov ecx, num1 
+  mov edx, 2
+  int 0x80            
 
+  mov eax, SYS_WRITE        
+  mov ebx, STDOUT
+  mov ecx, msg2          
+  mov edx, len2         
+  int 0x80
+
+  mov eax, SYS_READ  
+  mov ebx, STDIN  
+  mov ecx, num2 
+  mov edx, 2
+  int 0x80        
+
+  mov eax, SYS_WRITE        
+  mov ebx, STDOUT         
+  mov ecx, msg3          
+  mov edx, len3         
+  int 0x80
+  
 exit:
   mov eax, SYS_EXIT   
   xor ebx, ebx 
